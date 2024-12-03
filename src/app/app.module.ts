e7fc1564014bbbb17a43858dd7bfd9c18beb72eb
alejandrodgz <alejandro_garciaz13@outlook.com>
2024-12-03 09:26:28 -0500

    creation of folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    HomePageComponent,
+    SharedComponent
   ],
   imports: [
     BrowserModule,
